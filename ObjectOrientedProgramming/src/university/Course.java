fa6e9873c238780199f1905960ffddf175f1ea13
Luvienze <94405625+Luvienze@users.noreply.github.com>
2023-08-23 14:44:51 +0300

    Update Course.java
    I've changed major things. I changed methods to get/set for make them stateful.

--- ObjectOrientedProgramming/src/university/Course.java
+++ ObjectOrientedProgramming/src/university/Course.java
@@ -9,6 +9,33 @@ public class Course {
 	Professor teacher;
 	Student[] students;
 	String[] days;
+	Classroom classroom;
 	Date time;
+	
+	public void setCode (String newCode) {
+		code = newCode;
+	}
+	public void setName (String newName) {
+		name = newName;
+	}
+	public void setTeacher(Professor newTeacher) {
+		teacher = newTeacher;
+	}
+	public void setDepartment (Department newDepartment) {
+		department = newDepartment;
+	}
+	public void setClassroom (Classroom newClassroom) {
+		classroom = newClassroom;
+	}
+	public void setStudent (Student[] newStudents)
+	{
+		students = newStudents;
+	}
+	/*public void getStudent (Student newStudents) {
+		students = newStudents;
+	}*/
+	public void getDays (String[] newDays) {
+		days = newDays;
+	}
 
 }
