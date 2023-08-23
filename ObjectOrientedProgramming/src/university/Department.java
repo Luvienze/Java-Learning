25844ab5b3a18d186274ea3fa7bd7ed972f1595d
Luvienze <94405625+Luvienze@users.noreply.github.com>
2023-08-23 14:46:18 +0300

    Update Department.java
    I've changed major things. I've added get/set to methods for make them stateful.

--- ObjectOrientedProgramming/src/university/Department.java
+++ ObjectOrientedProgramming/src/university/Department.java
@@ -4,5 +4,27 @@ public class Department {
 	String name;
 	Professor head;
 	Course[] courses;
+	Course course;
 	Student[] students;
+	Classroom[] classrooms;
+	
+	public void setName( String newName) {
+		name = newName;
+	}
+	public void setProfessor (Professor newHead) {
+		head = newHead;
+	}
+	public void setCourse (Course[] newCourses) {
+		courses = newCourses;
+	}
+	public void getCourse (Course newCourse) {
+		course = newCourse;
+	}
+	public void setStudent (Student[] newStudents) {
+		students = newStudents;
+	}
+	public void setClassroom (Classroom[] newClassrooms) {
+		classrooms = newClassrooms;
+	}
+
 }
