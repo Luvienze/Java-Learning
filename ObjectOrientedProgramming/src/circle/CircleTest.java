9d328b822dec0a8d5f2a7d437c1766b35352af6f
Luvienze <94405625+Luvienze@users.noreply.github.com>
2023-08-24 23:06:11 +0300

    Update CircleTest.java
    I changed the method calls. Old version is still in the code.

--- ObjectOrientedProgramming/src/circle/CircleTest.java
+++ ObjectOrientedProgramming/src/circle/CircleTest.java
@@ -5,11 +5,23 @@ public class CircleTest {
 	public static void main(String[] args)
 	{
 		Scanner getUser = new Scanner(System.in);
-		Circle circle = new Circle();
+		//we can use this block without constructor method.
+		/*Circle circle = new Circle();
 		
 		System.out.println("Enter the radius of the circle : " + "(PI is " + Math.PI + ")");
 		double radius = getUser.nextDouble();
 		circle.areaOfCircle(radius);
-		circle.periemeterOfCircle(radius);
+		circle.periemeterOfCircle(radius);*/
+		
+		Circle circle1 = new Circle();
+		circle1.areaOfCircle(circle1.radius);
+		circle1.periemeterOfCircle(circle1.radius);
+		
+		Circle circle2 = new Circle(getUser.nextDouble());
+		circle2.areaOfCircle(circle2.radius);
+		circle2.periemeterOfCircle(circle2.radius);
+		
+		
 	}
 }
+
