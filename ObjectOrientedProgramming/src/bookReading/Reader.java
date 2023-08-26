e3a2d42d7b09c1c7c69c3faf4de3284c0a583447
Luvienze <94405625+Luvienze@users.noreply.github.com>
2023-08-26 15:19:50 +0300

    Update Reader.java
    I changed methods and add this. and this().
    I didn't delete the old version of code for later.

--- ObjectOrientedProgramming/src/bookReading/Reader.java
+++ ObjectOrientedProgramming/src/bookReading/Reader.java
@@ -5,9 +5,14 @@ public class Reader {
 	int age;
 	char sex;
 	Book book;
+	{
+		name = "Undefined User";
+		age = 20;
+		sex = 'F';
+	}
 	
 	
-	void setName (String newName) {
+	/*void setName (String newName) {
 		name = newName;
 	}
 	void setAge (int newAge) {
@@ -18,6 +23,19 @@ public class Reader {
 	}
 	void read (Book newBook) {
 		book = newBook;
+	}*/
+	void setName (String name) {
+		this.name = name;
 	}
+	void setAge (int age) {
+		this.age = age;
+	}
+	void setSex (char sex) {
+		this.sex = sex;
+	}
+	void read (Book book) {
+		this.book = book;
+	}
+	
 
 }
