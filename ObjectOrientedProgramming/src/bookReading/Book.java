48957aaca9e872bf263699ffc76be41ed06ab352
Luvienze <94405625+Luvienze@users.noreply.github.com>
2023-08-26 15:18:57 +0300

    Update Book.java
    I changed the methods and add this. and this().
    I didn't create initialization methods for Book.
    And didn't delete the old version of code for later.

--- ObjectOrientedProgramming/src/bookReading/Book.java
+++ ObjectOrientedProgramming/src/bookReading/Book.java
@@ -10,7 +10,7 @@ public class Book {
 	Reader currentReader;
 	int isFinished;
 	
-	void setTitle (String newTitle) {
+	/*void setTitle (String newTitle) {
 		title = newTitle;
 	}
 	void setPage (int newPage) {
@@ -36,9 +36,9 @@ public class Book {
 	void getPage (int newCurrentPage) {
 		currentPage = newCurrentPage;
 		System.out.println("Bookmarked " + currentPage);
-	}
+	}*/
 	void isFinished () {
-		if(page == currentPage)
+		if(this.page == this.currentPage)
 		{
 			System.out.println("Book is finished");
 		}
@@ -47,5 +47,32 @@ public class Book {
 			System.out.println("There are " + (page - currentPage) + " pages left to read.");
 		}
 	}
+	void setTitle (String title) {
+		this.title = title;
+	}
+	void setPage (int page) {
+		this.page = page;
+	}
+	void setType (String type) {
+		this.type = type;
+	}
+	void getAuthor (Author author) {
+		this.author = author;
+	}
+	void getReader (Reader currentReader) {
+		this.currentReader = currentReader;
+	}
+	void getPage (int currentPage) {
+		this.currentPage = currentPage;
+		System.out.println("Bookmarked " + currentPage);
+	}
+	boolean setCoverType (boolean coverType) {
+		if(this.isHardCover = coverType) {
+			return true;
+		}
+		else {
+			return false;
+		}
+}
 
 }
